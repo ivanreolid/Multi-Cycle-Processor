6eb296fc2072cc0a1614fcb3e05882a378df23b3
Ivan <ivan@DESKTOP-7H7H9LG.home>
2025-11-04 20:32:28 +0100

    WB stage generates register bank wires

--- wb_stage.sv
+++ wb_stage.sv
@@ -1,19 +1,24 @@
 import params_pkg::*;
 
 module wb_stage #(
-  parameter int DATA_WIDTH = params_pkg::DATA_WIDTH,
-  parameter int ADDR_WIDTH = params_pkg::ADDR_WIDTH
+  parameter int REGISTER_WIDTH = params_pkg::REGISTER_WIDTH,
+  parameter int DATA_WIDTH     = params_pkg::DATA_WIDTH,
+  parameter int ADDR_WIDTH     = params_pkg::ADDR_WIDTH
 )(
   input  logic clk_i,
   input  logic rst_i,
+  input  logic valid_i,
+  input  logic reg_wr_en_i,
+  input  logic [REGISTER_WIDTH-1:0] wr_reg_i,
   input  logic [DATA_WIDTH-1:0] alu_result_i,
   input  logic [DATA_WIDTH-1:0] data_from_mem_i,
   input  logic is_load_i,
 `ifndef SYNTHESIS
-  input logic  debug_valid_i,
   input  logic [ADDR_WIDTH-1:0] debug_pc_i,
   input  instruction_t debug_instr_i,
 `endif
+  output logic reg_wr_en_o,
+  output logic [REGISTER_WIDTH-1:0] wr_reg_o,
   output logic [DATA_WIDTH-1:0] data_to_reg_o,
 `ifndef SYNTHESIS
   output logic debug_instr_is_completed_o,
@@ -24,12 +29,14 @@ module wb_stage #(
 
   always_ff @(posedge clk_i) begin : flops
 `ifndef SYNTHESIS
-    debug_instr_is_completed_o <= debug_valid_i;
+    debug_instr_is_completed_o <= valid_i;
     debug_pc_o                 <= debug_pc_i;
     debug_instr_o              <= debug_instr_i;
 `endif
   end
 
+  assign reg_wr_en_o   = reg_wr_en_i & valid_i;
+  assign wr_reg_o      = wr_reg_i;
   assign data_to_reg_o = is_load_i ? data_from_mem_i : alu_result_i;
 
 endmodule : wb_stage
