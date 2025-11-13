4e5d66dead7ea2d2e2032f5432be6c7496d361ad
Ivan <ivan@DESKTOP-7H7H9LG.home>
2025-11-13 22:02:13 +0100

    Last EX stage result is flopped to WB stage

--- ex_stages.sv
+++ ex_stages.sv
@@ -36,45 +36,42 @@ module ex_stages #(
 
   always_ff @(posedge clk_i) begin : flops
     if (!rst_i) begin
-      pipe2_valid <= 1'b0;
-      pipe3_valid <= 1'b0;
-      pipe4_valid <= 1'b0;
-      pipe5_valid <= 1'b0;
+      pipe2_valid    <= 1'b0;
+      pipe3_valid    <= 1'b0;
+      pipe4_valid    <= 1'b0;
+      pipe5_valid    <= 1'b0;
+      result_ready_o <= 1'b0;
     end else begin
-      pipe2_valid  <= valid_i;
-      pipe3_valid  <= pipe2_valid;
-      pipe4_valid  <= pipe3_valid;
-      pipe5_valid  <= pipe4_valid;
-      pipe2_wr_reg <= wr_reg_i;
-      pipe3_wr_reg <= pipe2_wr_reg;
-      pipe4_wr_reg <= pipe3_wr_reg;
-      pipe5_wr_reg <= pipe4_wr_reg;
-      pipe2_result <= a_i * b_i;
-      pipe3_result <= pipe2_result;
-      pipe4_result <= pipe3_result;
-      pipe5_result <= pipe4_result;
+      pipe2_valid    <= valid_i;
+      pipe3_valid    <= pipe2_valid;
+      pipe4_valid    <= pipe3_valid;
+      pipe5_valid    <= pipe4_valid;
+      result_ready_o <= pipe5_valid;
+      pipe2_wr_reg   <= wr_reg_i;
+      pipe3_wr_reg   <= pipe2_wr_reg;
+      pipe4_wr_reg   <= pipe3_wr_reg;
+      pipe5_wr_reg   <= pipe4_wr_reg;
+      wr_reg_o       <= pipe5_wr_reg;
+      pipe2_result   <= a_i * b_i;
+      pipe3_result   <= pipe2_result;
+      pipe4_result   <= pipe3_result;
+      pipe5_result   <= pipe4_result;
+      result_o       <= pipe5_result;
 `ifndef SYNTHESIS
       pipe2_debug_pc    <= debug_pc_i;
       pipe3_debug_pc    <= pipe2_debug_pc;
       pipe4_debug_pc    <= pipe3_debug_pc;
       pipe5_debug_pc    <= pipe4_debug_pc;
+      debug_pc_o        <= pipe5_debug_pc;
       pipe2_debug_instr <= debug_instr_i;
       pipe3_debug_instr <= pipe2_debug_instr;
       pipe4_debug_instr <= pipe3_debug_instr;
       pipe5_debug_instr <= pipe4_debug_instr;
+      debug_instr_o     <= pipe5_debug_instr;
 `endif
     end
   end
 
-  assign wb_is_next_cycle_o = pipe4_valid;
-
-  assign result_ready_o = pipe5_valid;
-  assign wr_reg_o       = pipe5_wr_reg;
-  assign result_o       = pipe5_result;
-
-`ifndef SYNTHESIS
-  assign debug_pc_o    = pipe5_debug_pc;
-  assign debug_instr_o = pipe5_debug_instr;
-`endif
+  assign wb_is_next_cycle_o = pipe5_valid;
 
 endmodule : ex_stages
